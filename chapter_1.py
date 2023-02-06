434db848ac5a3fa97695add627fe6d0699833f0d
Inaam Ahmed <113410677+InaamKhan11@users.noreply.github.com>
2023-02-06 10:43:01 +0500

    Add files via upload

---
+++ chapter_1.py
@@ -0,0 +1,51 @@
+print("hello dear\nhello world")
+print("hello")
+print('hello')
+a = 1212312312
+b = 50
+c = 100
+print(a, b, c)
+
+# integer  int
+# string  str
+# float  2.5234234
+# bool  bool
+
+a = 10
+b = "asd"
+c = 3.4
+print(type(a))
+print(type(b))
+print(type(c))
+print(10 == 10)
+
+e = input("please enter")
+print(e)
+
+a = input("please enter your name")
+print("Your name is ",a,"\n thank you for enter your name",a)
+
+a = input("enter the number 1")
+b = input("enter the number 2")
+c = a + b
+print(c)
+
+a = int(input("enter the number 1"))
+b = int(input("enter the number 2"))
+c = a + b
+print(c)
+
+a = "5"
+a = int(a)
+b = "5.7"
+b = float(b)
+print(type(a))
+print(type(b))
+
+print(10 + 10)
+
+# Assignment
+# your name is (this)
+# your age is (this)
+# hello (name)
+# your age (age)
